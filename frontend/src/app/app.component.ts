839a1d53e0ed8b8435a3becd635886b4856d7862
harrybandukda <harrisbandukda@gmail.com>
2025-04-07 00:33:39 +0500

    app comp added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterModule, CommonModule, RouterOutlet],
+  template: `
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Employee Management System';
 }
